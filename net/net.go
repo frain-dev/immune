a5773ee84391c467fee341a87cd0107e18a08088
Daniel Oluojomu <danvixent@gmail.com>
2022-02-19 22:56:37 +0100

    add url package & improve net

--- net/net.go
+++ net/net.go
@@ -7,24 +7,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-
-	"github.com/sirupsen/logrus"
-
-	"github.com/frain-dev/immune/callback"
+	"time"
 
 	"github.com/frain-dev/immune"
+	"github.com/frain-dev/immune/callback"
+	"github.com/frain-dev/immune/net/url"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 type Net struct {
-	client  *http.Client
-	s       *callback.Server
-	baseURL string
+	s                      *callback.Server
+	client                 *http.Client
+	vm                     *immune.VariableMap
+	maxCallbackWaitSeconds int
+	baseURL                string
 }
 
-func (n *Net) ExecuteSetupTestCase(ctx context.Context, setupTC *immune.SetupTestCase, vm immune.VariableMap) error {
+func (n *Net) ExecuteSetupTestCase(ctx context.Context, setupTC *immune.SetupTestCase) error {
+	u, err := url.Parse(fmt.Sprintf("%s%s", n.baseURL, setupTC.Endpoint))
+	if err != nil {
+		return errors.Wrapf(err, "setup_test_case %d: failed to parse url", setupTC.Position)
+	}
+
+	result, err := u.ProcessWithVariableMap(n.vm)
+	if err != nil {
+		return errors.Wrapf(err, "setup_test_case %d: failed to process parsed url with variable map", setupTC.Position)
+	}
+
 	r := &request{
 		body:   setupTC.RequestBody,
-		url:    fmt.Sprintf("%s%s", n.baseURL, setupTC.Endpoint),
+		url:    result,
 		method: setupTC.HTTPMethod,
 	}
 
@@ -33,14 +46,9 @@ func (n *Net) ExecuteSetupTestCase(ctx context.Context, setupTC *immune.SetupTes
 		return err
 	}
 
-	report := &immune.SetupTestCaseReport{
-		WantsResponseBody: setupTC.ResponseBody,
-	}
-
 	if setupTC.ResponseBody {
 		if len(resp.body) == 0 {
-			report.HasResponseBody = false
-			setupTC.Report = report
+			return errors.Wrapf(err, "setup_test_case %d: wants response body but got no response body", setupTC.Position)
 		}
 
 		m := immune.M{}
@@ -49,22 +57,76 @@ func (n *Net) ExecuteSetupTestCase(ctx context.Context, setupTC *immune.SetupTes
 			return err
 		}
 
-		err = vm.ProcessResponse(ctx, setupTC.StoreResponseVariables, m)
-		if err != nil {
-			return err
+		if setupTC.StoreResponseVariables != nil {
+			err = n.vm.ProcessResponse(ctx, setupTC.StoreResponseVariables, m)
+			if err != nil {
+				return err
+			}
 		}
 	} else {
 		if len(resp.body) > 0 {
-			report.HasResponseBody = true
-			setupTC.Report = report
+			return errors.Wrapf(err, "setup_test_case %d: does not want a response body but got a response body: '%s'", setupTC.Position, string(resp.body))
 		}
 	}
 
 	return nil
 }
 
-func (n *Net) ExecuteTestCase(ctx context.Context, tc *immune.TestCase) {
+func (n *Net) ExecuteTestCase(ctx context.Context, tc *immune.TestCase) error {
+	u, err := url.Parse(fmt.Sprintf("%s%s", n.baseURL, tc.Endpoint))
+	if err != nil {
+		return errors.Wrapf(err, "test_case %d: failed to parse url", tc.Position)
+	}
 
+	result, err := u.ProcessWithVariableMap(n.vm)
+	if err != nil {
+		return errors.Wrapf(err, "test_case %d: failed to process parsed url with variable map", tc.Position)
+	}
+
+	r := &request{
+		body:   tc.RequestBody,
+		url:    result,
+		method: tc.HTTPMethod,
+	}
+
+	resp, err := n.sendRequest(ctx, r)
+	if err != nil {
+		return err
+	}
+
+	if tc.ResponseBody {
+		if len(resp.body) == 0 {
+			return errors.Wrapf(err, "test_case %d: wants response body but got no response body", tc.Position)
+		}
+
+		m := immune.M{}
+		err = resp.Decode(&m)
+		if err != nil {
+			return err
+		}
+	} else {
+		if len(resp.body) > 0 {
+			return errors.Wrapf(err, "test_case %d: does not want a response body but got a response body: '%s'", tc.Position, string(resp.body))
+		}
+	}
+
+	if tc.Callback.Enabled {
+		cctx, cancel := context.WithTimeout(context.Background(), time.Duration(n.maxCallbackWaitSeconds)*time.Second)
+		defer cancel()
+
+		for i := 1; i <= tc.Callback.Times; i++ {
+			select {
+			case <-cctx.Done():
+				log.Infof("succesfully received %d callbacks for test_case %d before max callback wait seconds elapsed", i, tc.Position)
+				break
+			default:
+				n.s.ReceiveCallback()
+				log.Infof("callback %d for test_case %d received", i, tc.Position)
+			}
+		}
+	}
+
+	return nil
 }
 
 type request struct {
@@ -74,7 +136,8 @@ type request struct {
 }
 
 type response struct {
-	body []byte
+	statusCode int
+	body       []byte
 }
 
 func (resp *response) Decode(out interface{}) error {
@@ -83,6 +146,10 @@ func (resp *response) Decode(out interface{}) error {
 
 func (n *Net) sendRequest(ctx context.Context, r *request) (*response, error) {
 	rb, err := json.Marshal(r.body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal request body")
+	}
+
 	req, err := http.NewRequestWithContext(ctx, r.method.String(), r.url, bytes.NewBuffer(rb))
 	if err != nil {
 		return nil, err
@@ -94,15 +161,11 @@ func (n *Net) sendRequest(ctx context.Context, r *request) (*response, error) {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		logrus.WithField("status", resp.StatusCode).Infof("non ok status code returned")
-	}
-
 	var buf []byte
 	buf, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	return &response{body: buf}, nil
+	return &response{body: buf, statusCode: resp.StatusCode}, nil
 }
---
+++ net/url/url.go
@@ -0,0 +1,81 @@
+package url
+
+import (
+	"strings"
+
+	"github.com/frain-dev/immune"
+	"github.com/pkg/errors"
+)
+
+type URL struct {
+	segments []*segment
+	url      string
+}
+
+func Parse(s string) (*URL, error) {
+	if len(s) == 0 {
+		return nil, errors.New("url is empty")
+	}
+
+	u := &URL{segments: []*segment{}}
+
+	for {
+		seg := nextSegment(s)
+		if seg == nil {
+			break
+		}
+
+		u.segments = append(u.segments, seg)
+		s = s[seg.end:]
+	}
+
+	return u, nil
+}
+
+func (u *URL) ProcessWithVariableMap(vm *immune.VariableMap) (string, error) {
+	var result string
+
+	url := u.url
+	for _, s := range u.segments {
+		v, ok := vm.GetString(s.name)
+		if !ok {
+			return "", errors.Errorf("variable %s not found in variable map", s.name)
+		}
+
+		result = strings.Replace(url, url[s.start:s.end+1], v, -1)
+		url = url[s.end+1:]
+	}
+
+	return result, nil
+}
+
+type segment struct {
+	start int
+	end   int
+	name  string
+}
+
+func nextSegment(s string) *segment {
+	open := strings.IndexRune(s, '{')
+	if open < 0 {
+		return nil
+	}
+
+	close := open
+	for i, c := range s[open:] {
+		if c == '}' {
+			close = open + i
+			break
+		}
+	}
+
+	if close == open {
+		panic("url: variable closing delimiter '}' is missing")
+	}
+
+	return &segment{
+		start: open,
+		end:   close,
+		name:  s[open+1 : close],
+	}
+}
