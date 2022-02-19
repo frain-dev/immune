00e7758dd1d5608af2c803511c8d5468703633ed
Daniel Oluojomu <danvixent@gmail.com>
2022-02-19 22:55:29 +0100

    add more fields to setup test case & test case

--- system.go
+++ system.go
@@ -1,19 +1,21 @@
 package immune
 
 type System struct {
-	BaseURL        string          `json:"base_url"`
-	Variables      VariableMap     `json:"-"`
-	SetupTestCases []SetupTestCase `json:"setup_test_cases"`
-	TestCases      []TestCase      `json:"test_cases"`
+	BaseURL                string          `json:"base_url"`
+	MaxCallbackWaitSeconds int             `json:"max_callback_wait_seconds"`
+	Variables              VariableMap     `json:"-"`
+	SetupTestCases         []SetupTestCase `json:"setup_test_cases"`
+	TestCases              []TestCase      `json:"test_cases"`
 }
 
 type SetupTestCase struct {
-	StoreResponseVariables S                    `json:"store_response_variables"`
-	RequestBody            M                    `json:"request_body"`
-	ResponseBody           bool                 `json:"response_body"`
-	Endpoint               string               `json:"endpoint"`
-	HTTPMethod             Method               `json:"http_method"`
-	Report                 *SetupTestCaseReport `json:"-"`
+	StoreResponseVariables S      `json:"store_response_variables"`
+	RequestBody            M      `json:"request_body"`
+	ResponseBody           bool   `json:"response_body"`
+	Endpoint               string `json:"endpoint"`
+	HTTPMethod             Method `json:"http_method"`
+	Position               int    `json:"-"`
+	//Report                 *SetupTestCaseReport `json:"-"`
 }
 
 type SetupTestCaseReport struct {
@@ -22,6 +24,7 @@ type SetupTestCaseReport struct {
 }
 
 type TestCase struct {
+	Position     int      `json:"-"`
 	HTTPMethod   Method   `json:"http_method"`
 	Endpoint     string   `json:"endpoint"`
 	ResponseBody bool     `json:"response_body"`
