8d9190158ebdd9ae872e52513dceea14a1913c90
Daniel Oluojomu <danvixent@gmail.com>
2022-02-21 16:03:31 +0100

    allow for referencing nested fields

--- variable_map.go
+++ variable_map.go
@@ -3,6 +3,7 @@ package immune
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -32,9 +33,35 @@ func (v VariableMap) Get(key string) (interface{}, bool) {
 
 func (v VariableMap) ProcessResponse(ctx context.Context, variableToField S, resp M) error {
 	for varName, field := range variableToField {
-		value, ok := resp[field]
-		if !ok {
-			return errors.Errorf("variable %s's field %s not found in response", varName, field)
+
+		var value interface{}
+		var ok bool
+
+		// we may have separators referencing deeper fields in the response body e.g data.uid
+		parts := strings.Split(field, ".")
+
+		if len(parts) == 0 {
+			value, ok = resp[field]
+			if !ok {
+				return errors.Errorf("variable %s's field %s not found in response", varName, field)
+			}
+		} else {
+			next := M{}
+			lastPart := parts[len(parts)-1]
+			parts = parts[:len(parts)-1]
+
+			track := ""
+			for _, part := range parts {
+				v := resp[part]
+
+				next, ok = v.(map[string]interface{})
+				if !ok {
+					return errors.Errorf("the field %s, is not an object in response body", track)
+				}
+				track += part + "."
+			}
+
+			value = next[lastPart] // we have reached the last part of the "data.uid"
 		}
 
 		// only supporting string and int for now
